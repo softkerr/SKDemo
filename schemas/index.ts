9d5d8a75d971466734694a9eafe63f0f5e2d4426
bakryu <stasbakryu@gmail.com>
2023-07-05 14:00:00 +0300

    Setup Sanity schema index

---
+++ schemas/index.ts
@@ -0,0 +1,9 @@
+import service from './service';
+import pricingTier from './pricingTier';
+import teamMember from './teamMember';
+import siteSettings from './siteSettings';
+import page from './page';
+import navigation from './navigation';
+import products from './products';
+
+export const schemaTypes = [service, pricingTier, teamMember, siteSettings, page, navigation, products];
